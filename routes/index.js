cb2d0e032bd8e1a4e8ab91a1d0759dee4a8e4480
Mishaal.G <82124159+MishaalGh@users.noreply.github.com>
2024-11-13 18:02:37 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+// Names: Mishaal.G and Casey.P
+// Date: Nov.12.2024
+// Class: Web and Script Programming
+// Purpose: This is responsible for rendering the main page and exporting it.
 
-/* GET home page. */
+var express = require('express'); // Imports express for module creation
+var router = express.Router(); // New router instance creation 
+
+// Render the main page
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' }); 
 });
 
+// Exporets the router 
 module.exports = router;
+
+// End of program
