d5103f2a366ef583d0801a94359f8753b54bae72
Mishaal.G <82124159+MishaalGh@users.noreply.github.com>
2024-11-13 18:08:01 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
+// Names: Mishaal.G and Casey.P
+// Date: Nov.12.2024
+// Class: Web and Script Programming
+// Purpose: This is responsible for defining a route for GET request with a messege and exports.
+
+// Imports express for creating routs and creates a new instance in router
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+//Route  to get list of users
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// Exports the router  
 module.exports = router;
+
+// End of program
